53c0cbb5ef1b471b05960e87a08916e005317ca4
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-05-24 16:00:11 -0300

    Tela Home

---
+++ src/components/TextHome.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/home.css';
+
+function TextHome() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <p className="home__text">
+            Sou Engenheiro de Software <b>Front-End</b>.
+          </p>
+          <p className="home__text">
+            Graduado na primeira turma do curso de&nbsp;
+            <b>Sistemas para Internet</b> pela Fatec de Araras-SP e esse é meu
+            portfólio.
+          </p>
+          <p className="home__text">E esse é meu portfólio.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TextHome;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import FooterHome from '../components/FooterHome';
+import HeaderHome from '../components/HeaderHome';
+import ImageHome from '../components/ImageHome';
+import TextHome from '../components/TextHome';
+import '../css/home.css';
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="home">
+        <div className="container">
+          <HeaderHome />
+
+          <TextHome />
+
+          <ImageHome />
+
+          <FooterHome />
+        </div>
+      </div>
     </>
   );
 }
