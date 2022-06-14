e076532b5df56a79a6069f63e785bbb8c60bcd3f
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-06-14 10:41:17 -0300

    Troca BrowserRouter por HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import ProjectPage from './pages/Project/ProjectPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="projetos" element={<ProjectPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
